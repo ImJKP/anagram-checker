520b9c48881427880778a4d7d36d4b48a4e96c28
Jordan Phillips <jordankphillips@gmail.com>
2016-09-02 18:42:01 -0700

    Anagram checker code

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,53 +1,20 @@
-var sectionHeight = function() {
-  var total    = $(window).height(),
-      $section = $('section').css('height','auto');
+function checkAnagrams() {
+// Check if two strings are anagrams.
 
-  if ($section.outerHeight(true) < total) {
-    var margin = $section.outerHeight(true) - $section.height();
-    $section.height(total - margin - 20);
+  var firstString = prompt("What's the first string?");
+  var secondString = prompt("What's the second string?");
+
+  if (firstString.toLowerCase() == secondString.toLowerCase()) { alert("Those strings are the same!");
   } else {
-    $section.css('height','auto');
-  }
+
+    var simplify = function(string) {
+      return string.toLowerCase().split('').sort().join()
+    }
+
+    if ( simplify(firstString) === simplify(secondString) ) {
+        alert("Those strings are anagrams!")
+      } else {
+        alert("Those strings are NOT anagrams!")
+    }
+  }  
 }
-
-$(window).resize(sectionHeight);
-
-$(document).ready(function(){
-  $("section h1, section h2").each(function(){
-    $("nav ul").append("<li class='tag-" + this.nodeName.toLowerCase() + "'><a href='#" + $(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g,'') + "'>" + $(this).text() + "</a></li>");
-    $(this).attr("id",$(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g,''));
-    $("nav ul li:first-child a").parent().addClass("active");
-  });
-  
-  $("nav ul li").on("click", "a", function(event) {
-    var position = $($(this).attr("href")).offset().top - 190;
-    $("html, body").animate({scrollTop: position}, 400);
-    $("nav ul li a").parent().removeClass("active");
-    $(this).parent().addClass("active");
-    event.preventDefault();    
-  });
-  
-  sectionHeight();
-  
-  $('img').load(sectionHeight);
-});
-
-fixScale = function(doc) {
-
-  var addEvent = 'addEventListener',
-      type = 'gesturestart',
-      qsa = 'querySelectorAll',
-      scales = [1, 1],
-      meta = qsa in doc ? doc[qsa]('meta[name=viewport]') : [];
-
-  function fix() {
-    meta.content = 'width=device-width,minimum-scale=' + scales[0] + ',maximum-scale=' + scales[1];
-    doc.removeEventListener(type, fix, true);
-  }
-
-  if ((meta = meta[meta.length - 1]) && addEvent in doc) {
-    fix();
-    scales = [.25, 1.6];
-    doc[addEvent](type, fix, true);
-  }
-};
